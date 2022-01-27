72823efaf50888e3c2a1ff28972a00ddc7832937
Kee Reel <c4rb0n_unit@protonmail.com>
2022-01-27 22:43:26 +0300

    Add x86_64 docker_compose override

--- manage/db_helper.py
+++ manage/db_helper.py
@@ -1,6 +1,5 @@
 import os
 import psycopg2
-from dotenv import load_dotenv
 
 
 def env(name):
@@ -10,6 +9,4 @@ def env(name):
 def open_db():
     return psycopg2.connect(database = env('DB_NAME'), user = env('DB_USER'), 
         password = env('DB_PASS'), host = env('DB_HOST'), port = env('DB_PORT'))
-
-load_dotenv()
 

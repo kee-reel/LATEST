c8595a8529ee10ec5d9e145a05eb0a23d083e5f7
Kee Reel <c4rb0n_unit@protonmail.com>
2022-01-29 07:44:05 +0000

    Split docker-compose into prod and dev

--- run_docker_compose.sh
+++ run_docker_compose.sh
@@ -1,1 +1,9 @@
-sudo docker-compose -f docker-compose.yml -f docker-compose.$(arch).yml $@
+# !/bin/bash
+if (( $# == 0 )); then
+	echo "Usage: $0 [dev | prod] commands"
+	exit
+fi
+
+ARGS=($@)
+E=$1
+sudo docker-compose -f docker-compose.yml -f docker-compose.$(arch).yml -f docker-compose.$E.yml ${ARGS[@]:1}
